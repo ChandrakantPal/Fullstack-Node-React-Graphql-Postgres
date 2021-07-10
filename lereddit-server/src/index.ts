636e8471735a0ce33866bc064c6741bba83f0174
ChandrakantPal <sanjay161097@gmail.com>
2021-07-10 21:16:40 +0530

    server file

---
+++ lereddit-server/src/index.ts
@@ -0,0 +1,58 @@
+import 'reflect-metadata'
+import { MikroORM } from '@mikro-orm/core'
+import mikroOrmConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
+import redis from 'redis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import { __prod__ } from './constants'
+import { MyContext } from './types'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig)
+  await orm.getMigrator().up()
+
+  const app = express()
+
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: 'lax', //csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: '',
+      resave: false,
+    })
+  )
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  })
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('server started on http://localhost:4000')
+  })
+}
+
+main().catch((err) => {
+  console.error(err)
+})
