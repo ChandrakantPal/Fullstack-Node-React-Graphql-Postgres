81226289172a82f9b47519f9c4c3438ef26da1e5
ChandrakantPal <sanjay161097@gmail.com>
2021-07-10 21:15:56 +0530

    context type

---
+++ lereddit-server/src/types.ts
@@ -0,0 +1,11 @@
+import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core'
+import { Request, Response } from 'express'
+import session from 'express-session'
+
+export type MyContext = {
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+  req: Request & {
+    session: session.Session & Partial<session.SessionData> & any
+  }
+  res: Response
+}
