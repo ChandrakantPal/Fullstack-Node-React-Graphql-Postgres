d1df8b2ed325d411660d6c39e4fb9e7585d3eede
ChandrakantPal <sanjay161097@gmail.com>
2021-07-10 21:18:20 +0530

    index page

---
+++ lireddit-web/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { CTA } from '../components/CTA'
+import { Footer } from '../components/Footer'
+
+const Index = () => <div>hello world</div>
+
+export default Index
