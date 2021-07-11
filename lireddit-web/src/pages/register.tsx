b9ae8f505dea5ac41537b09c9f9a37b5f0b37dbf
ChandrakantPal <sanjay161097@gmail.com>
2021-07-11 16:55:39 +0530

    useRegisterMutation used

--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -3,14 +3,25 @@ import { Form, Formik } from 'formik'
 import Wrapper from '../components/Wrapper'
 import InputField from '../components/InputField'
 import { Box, Button } from '@chakra-ui/react'
+import { useRegisterMutation } from '../generated/graphql'
+import { toErrorMap } from '../utils/toErrorMap'
+import { useRouter } from 'next/router'
 
 const Register = () => {
+  const router = useRouter()
+  const [, register] = useRegisterMutation()
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.log(values)
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values)
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            // worked
+            router.push('/')
+          }
         }}
       >
         {({ isSubmitting }) => (
