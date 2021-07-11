70cae2fb7ed033f1cec0de2b1749b03259113e18
ChandrakantPal <sanjay161097@gmail.com>
2021-07-11 21:05:10 +0530

    urql provider removed

--- lireddit-web/src/pages/_app.tsx
+++ lireddit-web/src/pages/_app.tsx
@@ -1,28 +1,17 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { Provider, createClient } from 'urql'
-
 import theme from '../theme'
 
-const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  fetchOptions: {
-    credentials: 'include',
-  },
-})
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   )
 }
 
