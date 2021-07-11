03880ccb9be969af33826f29beff72aaf4cffe6b
ChandrakantPal <sanjay161097@gmail.com>
2021-07-11 16:49:53 +0530

    query builder

--- lereddit-server/src/resolvers/user.ts
+++ lereddit-server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Query,
 } from 'type-graphql'
 import argon2 from 'argon2'
+// import { EntityManager } from '@mikro-orm/postgresql'
 
 @InputType()
 class UsernamePasswordInput {
@@ -81,7 +82,19 @@ export class UserResolver {
       username: options.username,
       password: hashedPassword,
     })
+    // let user
     try {
+      // const result = await (em as EntityManager)
+      //   .createQueryBuilder(User)
+      //   .getKnexQuery()
+      //   .insert({
+      //     username: options.username,
+      //     password: hashedPassword,
+      //     created_at: new Date(),
+      //     updated_at: new Date(),
+      //   })
+      //   .returning('*')
+      // user = result[0]
       await em.persistAndFlush(user)
     } catch (err) {
       // duplicate username error
