05cb2035859793d1b378a4c0ddce5f51bc866556
ChandrakantPal <sanjay161097@gmail.com>
2021-07-11 21:06:52 +0530

    ssr + posts query

--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -1,19 +1,18 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { withUrqlClient } from 'next-urql'
+import Navbar from '../components/Navbar'
+import { usePostsQuery } from '../generated/graphql'
+import { createUrqlClient } from '../utils/createUrqlClient'
 
-const Index = () => <div>hello world</div>
+const Index = () => {
+  const [{ data }] = usePostsQuery()
+  return (
+    <>
+      <Navbar /> <div>hello world</div>
+      {!data
+        ? null
+        : data.posts.map((post) => <div key={post.id}>{post.title}</div>)}
+    </>
+  )
+}
 
-export default Index
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index)
