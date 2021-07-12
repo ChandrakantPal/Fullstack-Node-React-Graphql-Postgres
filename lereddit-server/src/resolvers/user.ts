1e5e781e034d1bd6468300a94dacc664be70c677
ChandrakantPal <sanjay161097@gmail.com>
2021-07-12 23:06:29 +0530

    forgot password mutation

--- lereddit-server/src/resolvers/user.ts
+++ lereddit-server/src/resolvers/user.ts
@@ -42,6 +42,12 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => Boolean)
+  async forgotPassword(@Arg('email') email: string, @Ctx() { em }: MyContext) {
+    // const user = await em.findOne(User, {email})
+    return true
+  }
+
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req, em }: MyContext) {
     //you are not logged in
