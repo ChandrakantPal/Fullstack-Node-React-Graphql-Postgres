9889926ef8390e542296a79277c4b8c6d40b1bca
ChandrakantPal <sanjay161097@gmail.com>
2021-07-14 22:40:05 +0530

    return shape changed

--- lereddit-server/src/util/ValidateRegister.ts
+++ lereddit-server/src/util/ValidateRegister.ts
@@ -2,44 +2,36 @@ import { UsernamePasswordInput } from 'src/resolvers/UsernamePasswordInput'
 
 export const validateRegister = (options: UsernamePasswordInput) => {
   if (!options.email.includes('@')) {
-    return {
-      errors: [
-        {
-          field: 'email',
-          message: 'invalid email',
-        },
-      ],
-    }
+    return [
+      {
+        field: 'email',
+        message: 'invalid email',
+      },
+    ]
   }
   if (options.username.length <= 2) {
-    return {
-      errors: [
-        {
-          field: 'username',
-          message: 'length must be greater than 2',
-        },
-      ],
-    }
+    return [
+      {
+        field: 'username',
+        message: 'length must be greater than 2',
+      },
+    ]
   }
   if (options.password.length <= 2) {
-    return {
-      errors: [
-        {
-          field: 'password',
-          message: 'length must be greater than 3',
-        },
-      ],
-    }
+    return [
+      {
+        field: 'password',
+        message: 'length must be greater than 3',
+      },
+    ]
   }
   if (options.username.includes('@')) {
-    return {
-      errors: [
-        {
-          field: 'username',
-          message: 'cannot inculde an @',
-        },
-      ],
-    }
+    return [
+      {
+        field: 'username',
+        message: 'cannot inculde an @',
+      },
+    ]
   }
 
   return null
