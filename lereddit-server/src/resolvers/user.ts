3b4470bd2f1bf2676c6d295823dd7f8d9981848c
ChandrakantPal <sanjay161097@gmail.com>
2021-07-14 23:35:38 +0530

    login erorr field changed

--- lereddit-server/src/resolvers/user.ts
+++ lereddit-server/src/resolvers/user.ts
@@ -117,7 +117,7 @@ export class UserResolver {
       return {
         errors: [
           {
-            field: 'username',
+            field: 'usernameOrEmail',
             message: "that username doesn't exist",
           },
         ],
