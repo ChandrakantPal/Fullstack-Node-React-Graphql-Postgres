16c9e328899376779593016728a8a720a29b2dcf
ChandrakantPal <sanjay161097@gmail.com>
2021-07-16 23:17:16 +0530

    redis type added

--- lereddit-server/src/types.ts
+++ lereddit-server/src/types.ts
@@ -1,11 +1,13 @@
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core'
 import { Request, Response } from 'express'
 import session from 'express-session'
+import { Redis } from 'ioredis'
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
   req: Request & {
     session: session.Session & Partial<session.SessionData> & any
   }
+  redis: Redis
   res: Response
 }
