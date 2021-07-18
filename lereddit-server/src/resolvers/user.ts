17ffb413b54c2d183e9f66ebfa2252cd351416b5
ChandrakantPal <sanjay161097@gmail.com>
2021-07-18 15:37:23 +0530

    updatedted the resolver according to typeorm style

--- lereddit-server/src/resolvers/user.ts
+++ lereddit-server/src/resolvers/user.ts
@@ -15,6 +15,7 @@ import { UsernamePasswordInput } from './UsernamePasswordInput'
 import { validateRegister } from '../util/ValidateRegister'
 import { sendEmail } from '../util/sendEmail'
 import { v4 } from 'uuid'
+import { getConnection } from 'typeorm'
 @ObjectType()
 class FieldError {
   @Field()
@@ -39,7 +40,7 @@ export class UserResolver {
   async changePassword(
     @Arg('token') token: string,
     @Arg('newPassword') newPassword: string,
-    @Ctx() { redis, em, req }: MyContext
+    @Ctx() { redis, req }: MyContext
   ): Promise<UserResponse> {
     if (newPassword.length <= 2) {
       return {
@@ -64,8 +65,8 @@ export class UserResolver {
         ],
       }
     }
-
-    const user = await em.findOne(User, { id: parseInt(userId) })
+    const userIdNum = parseInt(userId)
+    const user = await User.findOne(userIdNum)
 
     if (!user) {
       return {
@@ -78,10 +79,14 @@ export class UserResolver {
       }
     }
 
-    user.password = await argon2.hash(newPassword)
-    await em.persistAndFlush(user)
+    await User.update(
+      { id: userIdNum },
+      {
+        password: await argon2.hash(newPassword),
+      }
+    )
 
-    redis.del(key)
+    await redis.del(key)
 
     // log in user after change password
     req.session.userId = user.id
@@ -91,9 +96,9 @@ export class UserResolver {
   @Mutation(() => Boolean)
   async forgotPassword(
     @Arg('email') email: string,
-    @Ctx() { em, redis }: MyContext
+    @Ctx() { redis }: MyContext
   ) {
-    const user = await em.findOne(User, { email })
+    const user = await User.findOne({ where: { email } })
     if (!user) {
       // the email is not in db
       return true
@@ -114,45 +119,40 @@ export class UserResolver {
   }
 
   @Query(() => User, { nullable: true })
-  async me(@Ctx() { req, em }: MyContext) {
+  me(@Ctx() { req }: MyContext) {
     //you are not logged in
     if (!req.session.userId) {
       return null
     }
 
-    const user = await em.findOne(User, { id: req.session.userId })
-    return user
+    return User.findOne(req.session.userId)
   }
 
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em, req }: MyContext
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
     const errors = validateRegister(options)
     if (errors) {
       return { errors }
     }
     const hashedPassword = await argon2.hash(options.password)
-    const user = em.create(User, {
-      username: options.username,
-      email: options.email,
-      password: hashedPassword,
-    })
-    // let user
+    let user
     try {
-      // const result = await (em as EntityManager)
-      //   .createQueryBuilder(User)
-      //   .getKnexQuery()
-      //   .insert({
-      //     username: options.username,
-      //     password: hashedPassword,
-      //     created_at: new Date(),
-      //     updated_at: new Date(),
-      //   })
-      //   .returning('*')
-      // user = result[0]
-      await em.persistAndFlush(user)
+      // User.create({}).save()
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values({
+          username: options.username,
+          email: options.email,
+          password: hashedPassword,
+        })
+        .returning('*')
+        .execute()
+      user = result.raw[0]
     } catch (err) {
       // duplicate username error
       if (err.code === '23505') {
@@ -179,13 +179,12 @@ export class UserResolver {
   async login(
     @Arg('usernameOrEmail') usernameOrEmail: string,
     @Arg('password') password: string,
-    @Ctx() { em, req }: MyContext
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(
-      User,
+    const user = await User.findOne(
       usernameOrEmail.includes('@')
-        ? { email: usernameOrEmail }
-        : { username: usernameOrEmail }
+        ? { where: { email: usernameOrEmail } }
+        : { where: { username: usernameOrEmail } }
     )
     if (!user) {
       return {
