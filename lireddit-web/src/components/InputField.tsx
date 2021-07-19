a968c50cbba083b5dce8e0ab4881606e707ff770
ChandrakantPal <sanjay161097@gmail.com>
2021-07-19 22:53:25 +0530

    input or textarea

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -3,6 +3,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from '@chakra-ui/react'
 import { useField } from 'formik'
 import React, { FC, InputHTMLAttributes } from 'react'
@@ -10,14 +11,22 @@ import React, { FC, InputHTMLAttributes } from 'react'
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string
   name: string
+  textarea?: boolean
 }
 
-const InputField: FC<InputFieldProps> = ({ label, size: _, ...props }) => {
+const InputField: FC<InputFieldProps> = ({
+  label,
+  textarea,
+  size: _,
+  ...props
+}) => {
+  let InputOrTextarea: any = textarea ? Textarea : Input
+
   const [field, { error }] = useField(props)
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
+      <InputOrTextarea
         {...field}
         {...props}
         id={field.name}
