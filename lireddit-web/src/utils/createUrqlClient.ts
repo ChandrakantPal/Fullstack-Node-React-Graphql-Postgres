6ae6c474a0e565efbe348b8735737c3f511bec28
ChandrakantPal <sanjay161097@gmail.com>
2021-07-20 22:17:16 +0530

    createPost cache update

--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -133,6 +133,15 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          createPost: (_result, args, cache, info) => {
+            const allFields = cache.inspectFields('Query')
+            const fieldInfos = allFields.filter(
+              (info) => info.fieldName === 'posts'
+            )
+            fieldInfos.forEach((fieldInfo) => {
+              cache.invalidate('Query', 'posts', fieldInfo.arguments || {})
+            })
+          },
           logout: (_result, args, cache, info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
               cache,
