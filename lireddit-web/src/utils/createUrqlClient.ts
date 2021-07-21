c0c0206735336694ec618beb58cea920af174171
ChandrakantPal <sanjay161097@gmail.com>
2021-07-21 22:36:27 +0530

    cache update on vote

--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -12,9 +12,11 @@ import {
   MeDocument,
   LoginMutation,
   RegisterMutation,
+  VoteMutationVariables,
 } from '../generated/graphql'
 import { betterUpdateQuery } from './betterUpdateQuery'
 import Router from 'next/router'
+import gql from 'graphql-tag'
 
 export const errorExchange: Exchange =
   ({ forward }) =>
@@ -133,6 +135,29 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                }
+              `,
+              { id: postId } as any
+            ) // Data or null
+            if (data) {
+              const newPoints = (data.points as number) + value
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                  }
+                `,
+                { id: postId, points: newPoints } as any
+              )
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields('Query')
             const fieldInfos = allFields.filter(
