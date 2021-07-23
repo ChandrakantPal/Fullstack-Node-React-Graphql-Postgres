4a86ce3648858bd4845496adb16e11c78fc831b8
ChandrakantPal <sanjay161097@gmail.com>
2021-07-23 22:50:56 +0530

    delete post mutation updated

--- lereddit-server/src/resolvers/post.ts
+++ lereddit-server/src/resolvers/post.ts
@@ -167,8 +167,8 @@ export class PostResolver {
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg('id') id: number): Promise<Post | undefined> {
-    return Post.findOne(id)
+  post(@Arg('id', () => Int) id: number): Promise<Post | undefined> {
+    return Post.findOne(id, { relations: ['creator'] })
   }
 
   @Mutation(() => Post)
@@ -186,23 +186,45 @@ export class PostResolver {
   }
 
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
-    @Arg('id') id: number,
-    @Arg('title', () => String, { nullable: true }) title: string
+    @Arg('id', () => Int) id: number,
+    @Arg('title') title: string,
+    @Arg('text') text: string,
+    @Ctx() { req }: MyContext
   ): Promise<Post | null> {
-    const post = await Post.findOne(id)
-    if (!post) {
-      return null
-    }
-    if (typeof title !== 'undefined') {
-      await Post.update({ id }, { title })
-    }
-    return post
+    const result = await getConnection()
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title, text })
+      .where('id = :id and "creatorId" = :creatorId', {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning('*')
+      .execute()
+
+    return result.raw[0]
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg('id') id: number): Promise<boolean> {
-    await Post.delete(id)
+  @UseMiddleware(isAuth)
+  async deletePost(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { req }: MyContext
+  ): Promise<boolean> {
+    // not cascade way
+    // const post = await Post.findOne(id)
+    // if (!post) {
+    //   return false
+    // }
+    // if (post.creatorId !== req.session.userId) {
+    //   throw new Error('Not authorised')
+    // }
+    // await Updoot.delete({ postId: id })
+    // await Post.delete({ id })
+
+    await Post.delete({ id, creatorId: req.session.userId })
     return true
   }
 }
