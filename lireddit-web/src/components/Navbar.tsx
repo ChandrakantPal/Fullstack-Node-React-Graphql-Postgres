611809f97d1aab00ee807ceff595a9230c9effde
ChandrakantPal <sanjay161097@gmail.com>
2021-07-23 22:54:44 +0530

    create post button and heading moved to navbar

--- lireddit-web/src/components/Navbar.tsx
+++ lireddit-web/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Link } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { useLogoutMutation, useMeQuery } from '../generated/graphql'
 import { isServer } from '../utils/isServer'
@@ -31,7 +31,12 @@ const Navbar = () => {
   } else {
     // user is logged in
     body = (
-      <Flex>
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            Create Post
+          </Button>
+        </NextLink>
         <Box mr={2}>{data.me.username}</Box>
         <Button
           onClick={() => {
@@ -47,7 +52,14 @@ const Navbar = () => {
   }
   return (
     <Flex position="sticky" top={0} zIndex={2} bg="tan" p={4}>
-      <Box ml={'auto'}>{body}</Box>
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={'auto'}>{body}</Box>
+      </Flex>
     </Flex>
   )
 }
