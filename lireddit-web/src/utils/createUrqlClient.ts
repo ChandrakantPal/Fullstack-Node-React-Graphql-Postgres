7a34bdb36df08d3b7408d7632eed1fdb98eb68f8
ChandrakantPal <sanjay161097@gmail.com>
2021-07-23 22:54:09 +0530

    cache update on delete

--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -13,6 +13,7 @@ import {
   LoginMutation,
   RegisterMutation,
   VoteMutationVariables,
+  DeletePostMutationVariables,
 } from '../generated/graphql'
 import { betterUpdateQuery } from './betterUpdateQuery'
 import Router from 'next/router'
@@ -146,6 +147,12 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
         },
         updates: {
           Mutation: {
+            deletePost: (_result, args, cache, info) => {
+              cache.invalidate({
+                __typename: 'Post',
+                id: (args as DeletePostMutationVariables).id,
+              })
+            },
             vote: (_result, args, cache, info) => {
               const { postId, value } = args as VoteMutationVariables
               const data = cache.readFragment(
