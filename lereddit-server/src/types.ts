236d58f3f2224169332f7144548c8b48a96a49fe
ChandrakantPal <sanjay161097@gmail.com>
2021-07-26 20:08:53 +0530

    user loader and updoot loader

--- lereddit-server/src/types.ts
+++ lereddit-server/src/types.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import session from 'express-session'
 import { Redis } from 'ioredis'
+import { createUpdootLoader } from './util/createUpdootLoader'
+import { createUserLoader } from './util/createUserLoader'
 
 export type MyContext = {
   req: Request & {
@@ -8,4 +10,6 @@ export type MyContext = {
   }
   redis: Redis
   res: Response
+  userLoader: ReturnType<typeof createUserLoader>
+  updootLoader: ReturnType<typeof createUpdootLoader>
 }
