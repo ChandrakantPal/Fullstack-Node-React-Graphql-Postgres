bda2a05acf2954baec20d66d742a20064c0303db
ChandrakantPal <sanjay161097@gmail.com>
2021-07-28 23:32:35 +0530

    server ready

--- lereddit-server/src/index.ts
+++ lereddit-server/src/index.ts
@@ -1,31 +1,51 @@
 import 'reflect-metadata'
-import { MikroORM } from '@mikro-orm/core'
-import mikroOrmConfig from './mikro-orm.config'
+import 'dotenv-safe/config'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from './resolvers/post'
 import { UserResolver } from './resolvers/user'
-import redis from 'redis'
+import Redis from 'ioredis'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
-import { __prod__ } from './constants'
-import { MyContext } from './types'
+import { COOKIE_NAME, __prod__ } from './constants'
+import cors from 'cors'
+import { createConnection } from 'typeorm'
+import { Post } from './entities/Post'
+import { User } from './entities/User'
+import path from 'path'
+import { Updoot } from './entities/Updoot'
+import { createUserLoader } from './util/createUserLoader'
+import { createUpdootLoader } from './util/createUpdootLoader'
 
+// 'keyboard cat',
 const main = async () => {
-  const orm = await MikroORM.init(mikroOrmConfig)
-  await orm.getMigrator().up()
+  const conn = await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: true,
+    // synchronize: true,
+    migrations: [path.join(__dirname, './migrations/*')],
+    entities: [Post, User, Updoot],
+  })
+  await conn.runMigrations()
 
   const app = express()
 
   const RedisStore = connectRedis(session)
-  const redisClient = redis.createClient()
-
+  const redis = new Redis(process.env.REDIS_URL)
+  app.set('proxy', 1)
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  )
   app.use(
     session({
-      name: 'qid',
-      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      name: COOKIE_NAME,
+      store: new RedisStore({ client: redis, disableTouch: true }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
@@ -33,7 +53,7 @@ const main = async () => {
         secure: __prod__, // cookie only works in https
       },
       saveUninitialized: false,
-      secret: '',
+      secret: process.env.SESSION_SECRET,
       resave: false,
     })
   )
@@ -43,12 +63,18 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+      redis,
+      userLoader: createUserLoader(),
+      updootLoader: createUpdootLoader(),
+    }),
   })
 
-  apolloServer.applyMiddleware({ app })
+  apolloServer.applyMiddleware({ app, cors: false })
 
-  app.listen(4000, () => {
+  app.listen(parseInt(process.env.PORT), () => {
     console.log('server started on http://localhost:4000')
   })
 }
