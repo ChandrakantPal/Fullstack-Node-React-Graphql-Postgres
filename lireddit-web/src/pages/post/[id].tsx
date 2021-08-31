c0797d63c0b94a338e741b47bd507b9467cd8bc9
ChandrakantPal <sanjay161097@gmail.com>
2021-08-31 22:48:32 +0530

    apollo mutation and query updated

--- lireddit-web/src/pages/post/[id].tsx
+++ lireddit-web/src/pages/post/[id].tsx
@@ -6,9 +6,9 @@ import { createUrqlClient } from '../../utils/createUrqlClient'
 import { useGetPostFormUrl } from '../../utils/useGetPostFromUrl'
 
 const Post = () => {
-  const [{ data, error, fetching }] = useGetPostFormUrl()
+  const { data, error, loading } = useGetPostFormUrl()
 
-  if (fetching) {
+  if (loading) {
     return (
       <Layout>
         <div>Loading...</div>
@@ -40,4 +40,4 @@ const Post = () => {
   )
 }
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Post)
+export default Post
