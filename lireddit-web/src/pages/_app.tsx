34df5c5734fbd6a354a9542627df56d3a6b5ded3
ChandrakantPal <sanjay161097@gmail.com>
2021-09-01 22:16:11 +0530

    new merge method for pagination and caching

--- lireddit-web/src/pages/_app.tsx
+++ lireddit-web/src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { PaginatedPosts } from '../generated/graphql'
 import theme from '../theme'
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_API_URL as string,
   credentials: 'include',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          posts: {
+            keyArgs: [],
+            merge(
+              existing: PaginatedPosts | undefined,
+              incoming: PaginatedPosts
+            ): PaginatedPosts {
+              return {
+                ...incoming,
+                posts: [...(existing?.posts || []), ...incoming.posts],
+              }
+            },
+          },
+        },
+      },
+    },
+  }),
 })
 
 function MyApp({ Component, pageProps }: any) {
