fe875adad3c68a66d8641f236eed6a0f73079ec6
ChandrakantPal <sanjay161097@gmail.com>
2021-09-02 23:09:58 +0530

    apollo provider removed

--- lireddit-web/src/pages/_app.tsx
+++ lireddit-web/src/pages/_app.tsx
@@ -3,44 +3,17 @@ import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
 import { PaginatedPosts } from '../generated/graphql'
 import theme from '../theme'
 
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL as string,
-  credentials: 'include',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(
-              existing: PaginatedPosts | undefined,
-              incoming: PaginatedPosts
-            ): PaginatedPosts {
-              return {
-                ...incoming,
-                posts: [...(existing?.posts || []), ...incoming.posts],
-              }
-            },
-          },
-        },
-      },
-    },
-  }),
-})
-
 function MyApp({ Component, pageProps }: any) {
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </ApolloProvider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   )
 }
 
