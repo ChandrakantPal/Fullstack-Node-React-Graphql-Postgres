0996ec19017e6aa89cbb7d089131ca3bc763ec4f
ChandrakantPal <sanjay161097@gmail.com>
2021-09-04 23:52:48 +0530

    cache updated

--- lireddit-web/src/pages/login.tsx
+++ lireddit-web/src/pages/login.tsx
@@ -3,7 +3,7 @@ import { Form, Formik } from 'formik'
 import Wrapper from '../components/Wrapper'
 import InputField from '../components/InputField'
 import { Box, Button, Flex, Link } from '@chakra-ui/react'
-import { useLoginMutation } from '../generated/graphql'
+import { MeDocument, MeQuery, useLoginMutation } from '../generated/graphql'
 import { toErrorMap } from '../utils/toErrorMap'
 import { useRouter } from 'next/router'
 import NextLink from 'next/link'
@@ -17,7 +17,19 @@ const Login = () => {
       <Formik
         initialValues={{ usernameOrEmail: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await login({ variables: values })
+          const response = await login({
+            variables: values,
+            update: (cache, { data }) => {
+              cache.writeQuery<MeQuery>({
+                query: MeDocument,
+                data: {
+                  __typename: 'Query',
+                  me: data?.login.user,
+                },
+              })
+              cache.evict({ fieldName: 'posts:{}' })
+            },
+          })
           if (response.data?.login.errors) {
             setErrors(toErrorMap(response.data.login.errors))
           } else if (response.data?.login.user) {
