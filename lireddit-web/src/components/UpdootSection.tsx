5596140f9026bcae8066a29163ae30dcbef31200
ChandrakantPal <sanjay161097@gmail.com>
2021-09-04 23:31:04 +0530

    update cache on up vote and down vote

--- lireddit-web/src/components/UpdootSection.tsx
+++ lireddit-web/src/components/UpdootSection.tsx
@@ -1,10 +1,54 @@
+import { ApolloCache } from '@apollo/client'
 import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import { Flex, IconButton } from '@chakra-ui/react'
+import { gql } from '@urql/core'
 import { FC, useState } from 'react'
-import { PostSnippetFragment, useVoteMutation } from '../generated/graphql'
+import {
+  PostSnippetFragment,
+  useVoteMutation,
+  VoteMutation,
+} from '../generated/graphql'
 
 interface UpdootSectionProps {
   post: PostSnippetFragment
+}
+
+const updateAfterVote = (
+  value: number,
+  postId: number,
+  cache: ApolloCache<VoteMutation>
+) => {
+  const data = cache.readFragment<{
+    id: number
+    points: number
+    voteStatus: number | null
+  }>({
+    id: 'Post:' + postId,
+    fragment: gql`
+      fragment _ on Post {
+        id
+        points
+        voteStatus
+      }
+    `,
+  })
+  if (data) {
+    if (data.voteStatus === value) {
+      return
+    }
+    const newPoints =
+      (data.points as number) + (!data.voteStatus ? 1 : 2) * value
+    cache.writeFragment({
+      id: 'Post:' + postId,
+      fragment: gql`
+        fragment __ on Post {
+          points
+          voteStatus
+        }
+      `,
+      data: { points: newPoints, voteStatus: value },
+    })
+  }
 }
 
 const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
@@ -27,6 +71,7 @@ const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
               postId: post.id,
               value: 1,
             },
+            update: (cache) => updateAfterVote(1, post.id, cache),
           })
           setLoadingState('not-loading')
         }}
@@ -48,6 +93,7 @@ const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
               postId: post.id,
               value: -1,
             },
+            update: (cache) => updateAfterVote(-1, post.id, cache),
           })
           setLoadingState('not-loading')
         }}
